6fee33fa8e2a53bc87b9dcc8c0d996b333d81357
Arkadiusz Stankiewicz <arkadiuszstankiewicz@MacBook-Pro-test.local>
2017-11-09 14:23:26 +0100

    actions with map

--- maps/ViewController.swift
+++ maps/ViewController.swift
@@ -7,12 +7,49 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //ustawianie coordynatow
+        let latitude: CLLocationDegrees = 27.175244
+        let longitude: CLLocationDegrees = 78.0399773
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        //dodawanie regionu ktory ma sie wyswietlic do mapy
+        map.setRegion(region, animated: true)
+        //dodawanie pinezki na mapie z tytulem i tekstem
+        let annotation = MKPointAnnotation()
+        annotation.title = "Taj Mahal"
+        annotation.subtitle = "One day i will go here"
+        
+        annotation.coordinate = location
+        map.addAnnotation(annotation)
+        
+        //dodawanie przez uzytkownika pinezki. dodajemy UI long press gesture recognizer (uilpgr)
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress(gestureRecognizer:)))
+        uilpgr.minimumPressDuration = 2
+        map.addGestureRecognizer(uilpgr)
+        
+    }
+    
+    @objc
+    func longpress(gestureRecognizer: UIGestureRecognizer){
+        let touchPoint = gestureRecognizer.location(in: self.map)
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "New place"
+        annotation.subtitle = "Maybe i will go here too"
+        map.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
